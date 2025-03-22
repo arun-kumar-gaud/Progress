1d9a51e1e93cbd3274bd67dd73251f7c3367571e
Arun Kumar <arunkumargaud121@gmail.com>
2025-03-22 17:39:33 +0530

    added new features

--- Sqlite_progress.dart
+++ Sqlite_progress.dart
@@ -6,12 +6,20 @@ int age;
 Profile(this.name,this.age);
 
 void show()=>print('Welcome: $name, Your age: $age');
+
+
+void details()=>print('Welcome: $name, Your age: $age');
+
 }
+
 
 
 void main(){
 
   Profile  yes = Profile('arun', 18);
   yes.show();
+  yes.details();
 
 }
+
+//
